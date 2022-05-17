67000a0eacc8bad82f731773825dda7e883ef1c9
D3cl4n <dpmurphy@wpi.edu>
2022-05-17 17:48:49 -0400

    setup

---
+++ src/analyzer.py
@@ -0,0 +1,7 @@
+import argument_parser as cli_parser
+
+def main():
+    cli_parser.parse_args()
+
+if __name__ == '__main__':
+    main()
---
+++ src/argument_parser.py
@@ -0,0 +1,4 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(usage="python analyzer.py --binary <binary>")
