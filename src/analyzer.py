49bc6e09d3595847bb3c049d19d52178211cab5f
D3cl4n <dpmurphy@wpi.edu>
2022-05-19 22:23:12 -0400

    parsing work

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,17 +1,15 @@
-import binascii
 from capstone import *
+from elftools.elf.elffile import ELFFile
 
 def search(binary):
     print("[+] Analyzing binary: {}".format(binary))
 
     opcodes = b""
     with open(binary, "rb") as f:
-        chunk = f.read(16)
-        while chunk:
-            
-            chunk = f.read(16)
+        elf = ELFFile(f)
+        code = elf.get_section_by_name(".text")
+        opcodes = code.data()
 
-    print(opcodes)
     md = Cs(CS_ARCH_X86, CS_MODE_64)
-    for byte in md.disasm(opcodes, 318):
+    for byte in md.disasm(opcodes, 0):
         print("0x%x: \t%s\t%s" % (byte.address, byte.mnemonic, byte.op_str))
