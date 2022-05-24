56f5f05ef6ce9086b8fe39f8f3d71513dbf4b22d
D3cl4n <dpmurphy@wpi.edu>
2022-05-24 19:46:01 -0400

    reporting 2

--- src/report.py
+++ src/report.py
@@ -3,10 +3,13 @@ class Bug:
         self.function_name = func_name
         self.bug_type = bug_type
         self.code_context = disass
+
+    def print_report(self, str):
+        print(str)
     
     def buffer_overflow(self):
         buffer_size = 0x0
         total_locals = 0x0
-        print(f"[+] Buffer overflow via function {self.function_name} with buffer of size {buffer_size}, total local vars make {total_locals} bytes")
+        self.print_report(f"[+] Buffer overflow via function {self.function_name} with buffer of size {buffer_size}, total local vars make {total_locals} bytes")
     
     
