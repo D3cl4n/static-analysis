2f026febb86ef0a701ec27ec467765578ef86cbf
D3cl4n <dpmurphy@wpi.edu>
2022-05-24 19:18:34 -0400

    adding in bug reporting

--- src/report.py
+++ src/report.py
@@ -1,5 +1,12 @@
 class Bug:
-    def __init__(self, desc):
-        self.desc = desc
+    def __init__(self, func_name, bug_type, disass):
+        self.function_name = func_name
+        self.bug_type = bug_type
+        self.code_context = disass
+    
+    def buffer_overflow(self):
+        buffer_size = 0x0
+        total_locals = 0x0
+        print(f"[+] Buffer overflow via function {self.function_name} with buffer of size {buffer_size}, total local vars make {total_locals} bytes")
     
     
